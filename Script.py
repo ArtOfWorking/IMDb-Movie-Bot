e6e33d5d75eea6168a9b51eacecc87a7438ead51
ArtOfWorking <123292005+ArtOfWorking@users.noreply.github.com>
2023-07-02 17:48:29 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,46 @@
+class script(object):
+    START_TXT = """👋 Hello {},
+
+Welcome To Rajfiles Movies Search Bot.
+
+𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍.Gᴇᴛ ᴀʟʟ Mᴏᴠɪᴇs/ TV & Wᴇʙ sᴇʀɪᴇs/ Fᴀᴄᴛs/ Sᴏɴɢs etc.
+
+😊Mᴏᴠɪᴇ & Sᴇʀɪᴇs ʙʀᴏᴡsɪɴɢ ʜᴀs ɴᴇᴠᴇʀ ʙᴇᴇɴ ᴛʜɪs ᴇᴀsʏ."""
+
+    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
+★ Database: <a href=https://www.mongodb.com>MongoDB</a>
+★ Language: <a href=https://www.python.org>Python</a>
+★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
+
+    MY_OWNER_TXT = """★ Name: 『sᴀᴛʏᴀᴍ』
+★ Username: @Mr_majnu72
+★ ID: <code>5097836954</code>
+★ Country: India 🇮🇳"""
+
+    STATUS_TXT = """🗂 Total Files: <code>{}</code>
+👤 Total Users: <code>{}</code>
+👥 Total Chats: <code>{}</code>
+✨ Used Storage: <code>{}</code>
+⚡️ Free Storage: <code>{}</code>"""
+
+    NEW_GROUP_TXT = """#NewGroup
+★ Title: {}
+★ ID: <code>{}</code>
+★ Total Members: {}
+★ Added by: {}"""
+
+    NEW_USER_TXT = """#NewUser
+★ Name: {}
+★ ID: <code>{}</code>"""
+
+    NO_RESULT_TXT = """#NoResult
+★ Group Name: {}
+★ Group ID: <code>{}</code>
+★ Name: {}
+
+★ Message: {}"""
+
+    REQUEST_TXT = """★ Name: {}
+★ ID: <code>{}</code>
+
+★ Message: {}"""
